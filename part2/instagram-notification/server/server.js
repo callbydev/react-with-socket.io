a0c7b460b4411ac717812e577e916658bbe9d15d
tipup-manager <tipup-manager@gmail.com>
2023-02-22 22:49:20 +0900

    주석추가

--- part2/instagram-notification/server/server.js
+++ part2/instagram-notification/server/server.js
@@ -1,13 +1,18 @@
+// 1
 const { Server } = require("socket.io");
 const { posts } = require("./data");
+
+// 2
 const io = new Server("5000", {
   cors: {
     origin: "http://localhost:3000",
   },
 });
 
+// 3
 let users = [];
 
+// 4
 const addNewUser = (username, socketId) => {
   !users.some((user) => user.username === username) &&
     users.unshift({
@@ -17,16 +22,20 @@ const addNewUser = (username, socketId) => {
     });
 };
 
+// 5
 const getUser = (username) => {
   return users.find((user) => user.username === username);
 };
 
 io.on("connection", (socket) => {
+
+  // 6
   socket.on("newUser", (username) => {
     addNewUser(username, socket.id);
     io.sockets.emit("user-list", JSON.stringify(users));
   });
 
+  // 7
   socket.on("sendNotification", ({ senderName, receiverName, type }) => {
     const receiver = getUser(receiverName);
     io.to(receiver.socketId).emit("getNotification", {
