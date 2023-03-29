c4ad043a5aa7e585ce01523ae284d6b46d1eb4fc
HMG <jeongh@hyundai.com>
2023-03-29 13:45:59 +0900

    server

--- part1/nodejs/node-server-ex/server.js
+++ part1/nodejs/node-server-ex/server.js
@@ -1,10 +1,50 @@
+// 모듈 추출
 const http = require("http");
+const fs = require("fs").promises;
+const url = require("url");
 
-http
-  .createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.end("<h1>Hello NodeJs</h1>");
-  })
-  .listen(5000, () => {
-    console.log("Server is running");
-  });
+const users = [];
+
+// 서버 생성
+const server = http
+    .createServer(async (req, res) => {
+        const pathname = url.parse(req.url).pathname;
+        const method = req.method;
+        let data = null;
+
+        if (method === "GET") {
+            switch (pathname) {
+                case "/":
+                    res.writeHead(200, {
+                        "Content-Type": "text/html; charset=utf-8",
+                    });
+                    data = await fs.readFile("./index.html");
+                    res.end(data);
+                    break;
+                case "/users":
+                    try {
+                        res.setHeader("Content-Type", "application/json");
+                        res.end(JSON.stringify(users));
+                    } catch (err) {
+                        console.error(err);
+                    }
+                    break;
+                default:
+                    res.writeHead(400, {
+                        "Content-Type": "text/html; charset=utf-8",
+                    });
+                    data = await fs.readFile("./404.html");
+                    res.end(data);
+            }
+        }
+    })
+    .listen(52273);
+
+// 서버가 실행될 경우 이벤트 발생
+server.on("listening", () => {
+    console.log("52273 포트에서 서버가 실행중 입니다.");
+});
+// 서버에 에러가 발생할 경우 이벤트 발생
+server.on("error", (err) => {
+    console.log(err);
+});
