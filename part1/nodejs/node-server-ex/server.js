347b44d349072392552b85bc32ba03ecec5f8b58
tipup-manager <tipup-manager@gmail.com>
2023-04-01 14:09:06 +0900

    edit

--- part1/nodejs/node-server-ex/server.js
+++ part1/nodejs/node-server-ex/server.js
@@ -1,50 +1,45 @@
-// 모듈 추출
+// 1
 const http = require("http");
 const fs = require("fs").promises;
 const url = require("url");
 
-const users = [];
-
-// 서버 생성
+// 2
 const server = http
-    .createServer(async (req, res) => {
-        const pathname = url.parse(req.url).pathname;
-        const method = req.method;
-        let data = null;
+  .createServer(async (req, res) => {
+    // 3
+    const pathname = url.parse(req.url).pathname;
+    const method = req.method;
+    let data = null;
 
-        if (method === "GET") {
-            switch (pathname) {
-                case "/":
-                    res.writeHead(200, {
-                        "Content-Type": "text/html; charset=utf-8",
-                    });
-                    data = await fs.readFile("./index.html");
-                    res.end(data);
-                    break;
-                case "/users":
-                    try {
-                        res.setHeader("Content-Type", "application/json");
-                        res.end(JSON.stringify(users));
-                    } catch (err) {
-                        console.error(err);
-                    }
-                    break;
-                default:
-                    res.writeHead(400, {
-                        "Content-Type": "text/html; charset=utf-8",
-                    });
-                    data = await fs.readFile("./404.html");
-                    res.end(data);
-            }
-        }
-    })
-    .listen(52273);
+    console.log(url.parse(req.url));
 
-// 서버가 실행될 경우 이벤트 발생
+    
+    // 4
+    if (method === "GET") {
+      switch (pathname) {
+        case "/":
+          res.writeHead(200, {
+            "Content-Type": "text/html; charset=utf-8",
+          });
+          data = await fs.readFile("./index.html");
+          res.end(data);
+          break;
+        default:
+          res.writeHead(400, {
+            "Content-Type": "text/html; charset=utf-8",
+          });
+          data = await fs.readFile("./index.html");
+          res.end(data);
+      }
+    }
+  })
+  .listen(5000);
+
+// 5
 server.on("listening", () => {
-    console.log("52273 포트에서 서버가 실행중 입니다.");
+  console.log("5000 port is running");
 });
-// 서버에 에러가 발생할 경우 이벤트 발생
+// 6
 server.on("error", (err) => {
-    console.log(err);
+  console.log(err);
 });
