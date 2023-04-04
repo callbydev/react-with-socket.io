47213ffad8cf1f5bbf82cd37dfb1723086d97392
HMG <jeongh@hyundai.com>
2023-04-04 16:42:24 +0900

    수정

--- part1/socket/html5-websocket/server/server.js
+++ part1/socket/html5-websocket/server/server.js
@@ -3,25 +3,33 @@ const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 5000 });
 
 wss.on("connection", (ws) => {
-  const connectMsg = "새로운 사용자가 입장했습니다.";
-  console.log(connectMsg);
+    const broadCastHandler = (msg) => {
+        wss.clients.forEach(function each(client, i) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(msg);
+            }
+        });
+    };
 
-  const broadCastHandler = (msg) => {
-    wss.clients.forEach(function each(client, i) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(msg);
-      }
+    ws.on("message", (res) => {
+        const { type, data, id } = JSON.parse(res);
+        switch (type) {
+            case "id":
+                broadCastHandler(
+                    JSON.stringify({ type: "welcome", data: data })
+                );
+                break;
+            case "msg":
+                broadCastHandler(
+                    JSON.stringify({ type: "other", data: data, id: id })
+                );
+                break;
+            default:
+                break;
+        }
     });
-  };
 
-  broadCastHandler(connectMsg);
-
-  ws.on("message", (data) => {
-    console.log(`Client has sent us :${data}`);
-    broadCastHandler(data);
-  });
-
-  ws.on("close", () => {
-    console.log("client has disconnected");
-  });
+    ws.on("close", () => {
+        console.log("client has disconnected");
+    });
 });
