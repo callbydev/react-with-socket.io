5f097ded613edd154395e3d71deaa790be1e90b5
HMG <jeongh@hyundai.com>
2023-04-12 14:19:56 +0900

    수정

--- part2/google-docs/client/src/containers/editorContainer/EditorContainer.js
+++ part2/google-docs/client/src/containers/editorContainer/EditorContainer.js
@@ -1,150 +1,142 @@
 import React, { useEffect, useRef, useState } from "react";
 // 1
-import { io } from "socket.io-client";
 import { useParams } from "react-router-dom";
 import { debounce } from "lodash-es";
 import TextEditor from "../../components/textEditor/TextEditor";
+import { socket } from "../../socket";
 
 // 2
 const cursorMap = new Map();
 const cursorColor = [
-  "#FF0000",
-  "#FF5E00",
-  "#FFBB00",
-  "#FFE400",
-  "#ABF200",
-  "#1DDB16",
-  "#00D8FF",
-  "#0054FF",
+    "#FF0000",
+    "#FF5E00",
+    "#FFBB00",
+    "#FFE400",
+    "#ABF200",
+    "#1DDB16",
+    "#00D8FF",
+    "#0054FF",
 ];
 
 const EditorContainer = () => {
-  const socketIo = useRef(null);
-  const timerRef = useRef(null);
-  const cursorRef = useRef(null);
-  const reactQuillRef = useRef(null);
-  // 3
-  const { id: documentId } = useParams();
+    const timerRef = useRef(null);
+    const cursorRef = useRef(null);
+    const reactQuillRef = useRef(null);
+    // 3
+    const { id: documentId } = useParams();
 
-  const [text, setText] = useState("");
+    const [text, setText] = useState("");
 
-  // 4
-  useEffect(() => {
-    socketIo.current = io("http://localhost:5000");
-    return () => {
-      socketIo.current.disconnect();
+    // 4
+
+    // 5
+    useEffect(() => {
+        socket.emit("join", documentId);
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+
+    // 6
+    useEffect(() => {
+        socket.once("initDocument", (res) => {
+            const { _document, userList } = res;
+            setText(_document);
+            userList.forEach((u) => {
+                setCursor(u);
+            });
+        });
+    }, []);
+
+    // 7
+    useEffect(() => {
+        function setCursorHandler(user) {
+            setCursor(user);
+        }
+        socket.on("newUser", setCursorHandler);
+        return () => {
+            socket.off("newUser", setCursorHandler);
+        };
+    }, []);
+
+    // 8
+    useEffect(() => {
+        if (!reactQuillRef.current) return;
+        cursorRef.current = reactQuillRef.current
+            .getEditor()
+            .getModule("cursors");
+    }, []);
+
+    // 9
+    useEffect(() => {
+        function updateContentHandler(delta) {
+            reactQuillRef.current.getEditor().updateContents(delta);
+        }
+        socket.on("receive-changes", updateContentHandler);
+        return () => {
+            socket.off("receive-changes", updateContentHandler);
+        };
+    }, []);
+
+    // 10
+    useEffect(() => {
+        function updateHandler(res) {
+            const { range, id } = res;
+            debouncedUpdate(range, id);
+        }
+        socket.on("receive-cursor", updateHandler);
+        return () => {
+            socket.off("receive-cursor", updateHandler);
+        };
+    }, []);
+
+    // 11
+    const onChangeTextHandler = (content, delta, source, editor) => {
+        if (timerRef.current != null) {
+            clearTimeout(timerRef.current);
+        }
+        timerRef.current = setTimeout(() => {
+            socket.emit(
+                "save-document",
+                reactQuillRef.current.getEditor().getContents()
+            );
+            timerRef.current = null;
+        }, 1000);
+        if (source !== "user") return;
+        socket.emit("send-changes", delta);
     };
-  }, []);
 
-  // 5
-  useEffect(() => {
-    if (!socketIo.current) return;
-    socketIo.current.emit("join", documentId);
-  }, []);
-
-  // 6
-  useEffect(() => {
-    if (!socketIo.current) return;
-    socketIo.current.once("initDocument", (res) => {
-      const { _document, userList } = res;
-      setText(_document);
-      userList.forEach((u) => {
-        setCursor(u);
-      });
-    });
-  }, []);
-
-  // 7
-  useEffect(() => {
-    if (!socketIo.current) return;
-
-    socketIo.current.on("newUser", (user) => {
-      setCursor(user);
-    });
-    return () => {
-      socketIo.current.off("newUser");
-    };
-  }, []);
-
-  // 8
-  useEffect(() => {
-    if (!reactQuillRef.current) return;
-    cursorRef.current = reactQuillRef.current.getEditor().getModule("cursors");
-  }, []);
-
-  // 9
-  useEffect(() => {
-    if (!socketIo.current) return;
-
-    socketIo.current.on("receive-changes", (delta) => {
-      reactQuillRef.current.getEditor().updateContents(delta);
-    });
-    return () => {
-      socketIo.current.off("receive-changes");
-    };
-  }, []);
-
-  // 10
-  useEffect(() => {
-    if (!socketIo.current) return;
-
-    socketIo.current.on("receive-cursor", (res) => {
-      const { range, id } = res;
-      debouncedUpdate(range, id);
-    });
-    return () => {
-      socketIo.current.off("receive-cursor");
-    };
-  }, []);
-
-  // 11
-  const onChangeTextHandler = (content, delta, source, editor) => {
-    if (!socketIo.current) return;
-    if (timerRef.current != null) {
-      clearTimeout(timerRef.current);
+    // 12
+    function setCursor(id) {
+        if (!cursorMap.get(id)) {
+            cursorRef.current.createCursor(
+                id,
+                id,
+                cursorColor[Math.floor(Math.random() * 8)]
+            );
+            cursorMap.set(id, cursorRef.current);
+        }
     }
-    timerRef.current = setTimeout(() => {
-      socketIo.current.emit(
-        "save-document",
-        reactQuillRef.current.getEditor().getContents()
-      );
-      timerRef.current = null;
-    }, 1000);
-    if (source !== "user") return;
-    socketIo.current.emit("send-changes", delta);
-  };
 
-  // 12
-  function setCursor(id) {
-    if (!cursorMap.get(id)) {
-      cursorRef.current.createCursor(
-        id,
-        id,
-        cursorColor[Math.floor(Math.random() * 8)]
-      );
-      cursorMap.set(id, cursorRef.current);
-    }
-  }
+    // 13
+    const debouncedUpdate = debounce((range, id) => {
+        cursorMap.get(id).moveCursor(id, range);
+    }, 500);
 
-  // 13
-  const debouncedUpdate = debounce((range, id) => {
-    cursorMap.get(id).moveCursor(id, range);
-  }, 500);
+    // 14
+    const onChangeSelection = (selection, source, editor) => {
+        if (source !== "user") return;
+        socket.emit("cursor-changes", selection);
+    };
 
-  // 14
-  const onChangeSelection = (selection, source, editor) => {
-    if (source !== "user") return;
-    socketIo.current.emit("cursor-changes", selection);
-  };
-
-  return (
-    <TextEditor
-      text={text}
-      onChangeTextHandler={onChangeTextHandler}
-      onChangeSelection={onChangeSelection}
-      reactQuillRef={reactQuillRef}
-    />
-  );
+    return (
+        <TextEditor
+            text={text}
+            onChangeTextHandler={onChangeTextHandler}
+            onChangeSelection={onChangeSelection}
+            reactQuillRef={reactQuillRef}
+        />
+    );
 };
 
 export default EditorContainer;
--- part2/movie-theater/server/server.js
+++ part2/movie-theater/server/server.js
@@ -2,9 +2,9 @@ const { Server } = require("socket.io");
 const { seats } = require("./data");
 
 const io = new Server("5000", {
-  cors: {
-    origin: "http://localhost:3000",
-  },
+    cors: {
+        origin: "http://localhost:3000",
+    },
 });
 
 let avatar = [...seats];
@@ -12,63 +12,50 @@ let antman = [...seats];
 let cats = [...seats];
 
 const setSeats = (roomNumber, seat) => {
-  let temp = [];
-  if (roomNumber === "1") {
-    temp = [...avatar].map((s) => {
-      return s.map((i) => {
-        let temp = { ...i };
-        if (i.seatNumber === seat) {
-          temp = { ...i, status: 3 };
-        }
-        return temp;
-      });
-    });
-    avatar = [...temp];
-  } else if (roomNumber === "2") {
-    temp = [...antman].map((s) => {
-      return s.map((i) => {
-        let temp = { ...i };
-        if (i.seatNumber === seat) {
-          temp = { ...i, status: 3 };
-        }
-        return temp;
-      });
-    });
-    antman = [...temp];
-  } else {
-    temp = [...cats].map((s) => {
-      return s.map((i) => {
-        let temp = { ...i };
-        if (i.seatNumber === seat) {
-          temp = { ...i, status: 3 };
-        }
-        return temp;
-      });
-    });
-    cats = [...temp];
-  }
-  return temp;
+    let temp = [];
+    function setStatus(seats) {
+        return seats.map((i) => {
+            let temp = { ...i };
+            if (i.seatNumber === seat) {
+                temp = { ...i, status: 3 };
+            }
+            return temp;
+        });
+    }
+    if (roomNumber === "1") {
+        temp = [...avatar].map((s) => setStatus(s));
+        avatar = [...temp];
+    } else if (roomNumber === "2") {
+        temp = [...antman].map((s) => setStatus(s));
+        antman = [...temp];
+    } else {
+        temp = [...cats].map((s) => setStatus(s));
+        cats = [...temp];
+    }
+    return temp;
 };
 io.on("connection", (socket) => {
-  socket.on("join", (movie) => {
-    socket.join(movie);
-    let tempSeat = [];
-    if (movie === "1") {
-      tempSeat = avatar;
-    } else if (movie === "2") {
-      tempSeat = antman;
-    } else {
-      tempSeat = cats;
-    }
-    io.sockets.in(movie).emit("sSeatMessage", tempSeat);
-  });
+    socket.on("join", (movie) => {
+        socket.join(movie);
+        let tempSeat = [];
+        if (movie === "1") {
+            tempSeat = avatar;
+        } else if (movie === "2") {
+            tempSeat = antman;
+        } else {
+            tempSeat = cats;
+        }
+        io.sockets.in(movie).emit("sSeatMessage", tempSeat);
+    });
 
-  socket.on("addSeat", (seat) => {
-    const myRooms = Array.from(socket.rooms);
-    io.sockets.in(myRooms[1]).emit("sSeatMessage", setSeats(myRooms[1], seat));
-  });
+    socket.on("addSeat", (seat) => {
+        const myRooms = Array.from(socket.rooms);
+        io.sockets
+            .in(myRooms[1])
+            .emit("sSeatMessage", setSeats(myRooms[1], seat));
+    });
 
-  socket.on("disconnect", () => {
-    console.log("logout");
-  });
+    socket.on("disconnect", () => {
+        console.log("logout");
+    });
 });
