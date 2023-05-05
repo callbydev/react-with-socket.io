5c985e49a77b181374c6c4166c12dbf5e2b5855f
tipup-manager <tipup-manager@gmail.com>
2023-05-05 11:29:44 +0900

    수정

--- part2/slack2/client/src/App.js
+++ part2/slack2/client/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { IndexContainer, MainContainer } from "./containers";
 import { StoreProvider } from "./context";
